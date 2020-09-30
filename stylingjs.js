d158156d3b1dac3b1055ab9f9518de0aed32bc1d
manaswinisreekar <you@example.com>
2020-09-30 23:24:56 +0530

    Styling the Javascript

---
+++ stylingjs.js
@@ -0,0 +1,15 @@
+<!--Styling the Java Script-->
+<!DOCTYPE html>
+<html>
+<head>   
+   <title>Styling with Javascript</title>   
+</head>
+<body>
+   <p>Hi there,</p>
+   <p id="customPara">Change my color using Javascript</p>
+   <script>
+         /*Insert your code here*/
+         document.getElementById('customPara').style.margin='0';
+   </script>
+</body>
+</html>
